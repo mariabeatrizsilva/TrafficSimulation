d2f695b906378ade54c7b94caa04a595e0e65984
Maria Beatriz <ms14127@nyu.edu>
2024-04-23 13:28:21 -0400

    tried fixing typos????

--- setlights.m
+++ setlights.m
@@ -1,16 +1,16 @@
 if t > tlc
     for i = 1:ni
-        jgreen(i) = jgreen(i) + 1
+        jgreen(i) = jgreen(i) + 1;
         if jgreen(i) > nbin(i)
-            jgreen(i) = 1
+            jgreen(i) = 1;
         end
     end
-    tlc = tlc+tlcstep
+    tlc = tlc+tlcstep;
 end
 S = zeros(1,nb)
-for i 1:ni
-    b = bin(i,jgreen(i))
-    S(b) = 1
+for i = 1:ni
+    b = bin(i,jgreen(i));
+    S(b) = 1;
 end
 
 % Initialization for setlights
--- traffic.m
+++ traffic.m
@@ -1,6 +1,29 @@
-initialize
+% initialize
+clockmax = 100;
+hours = 24;
+dt  = hours/clockmax;
+tlc = 4*6; %% number of traffic lights?
+nb  = 38; %% number of blocks
+ni  = 4*6;
+nc  = 10; % number of cars
+jgreen = zeros(ni);
+
+%%Initializing some arrays 
+for i=1:ni
+    nbin(i) = sum(i2==i);
+    nbin(i) = sum(i1==i);
+end 
+nbinmax = max(bin);
+nboutmax = max(nbout)
+bin = zeros(ni, nbinmax)
+bout = zeros(ni, nboutmax)
+for (i=ni)
+    bin(i,1:nbin(i)) = find(i2==i);
+    bout(i,1:nbout(i))= find(i1==i);
+end 
+
 for clock = 1 : clockmax
-    t = clock * dt
+    t = clock * dt;
     setlights
     createcars
     morecars
